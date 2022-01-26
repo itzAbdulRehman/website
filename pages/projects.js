1899aa6365d4c77ac4d4b106a0d0c37872d9971e
AbdulRehman <itzabdulrehman786@gmail.com>
2022-01-26 17:17:47 +0500

    fix build

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-import { Container, Heading, SimpleGrid, Center } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/sections'
 import { ProjectsGridItem } from '../components/grid-item'
